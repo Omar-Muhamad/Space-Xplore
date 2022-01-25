3c1cdf6f1d766ab11d3b62d65d904a3ef97ccb70
George Magdy <georgtriple@gmail.com>
2022-01-25 07:42:57 +0200

    Refactor configure store

--- src/Redux/configreStore.js
+++ src/Redux/configreStore.js
@@ -4,11 +4,13 @@ import {
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import counterReducer from './counter/counter';
+import missionsReducer from './missions/missions';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducers = combineReducers({
   counter: counterReducer,
+  missions: missionsReducer,
 });
 
 const store = createStore(
