4d835c07fa517b7496b2391489e38d9c33b68a0d
George Magdy <georgtriple@gmail.com>
2022-01-25 07:41:38 +0200

    Make header responsive

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,14 +3,10 @@ import img from '../images/planet.png';
 
 const Header = () => (
   <div className="container">
-    <header className="header py-4 flex items-center justify-between px-12">
+    <header className="header py-4 flex items-center justify-center sm:justify-between px-12">
       <NavLink
-        activeClassName="test"
         to="/"
-        className={
-          (isActive) =>
-            `flex items-center gap-3 ${!isActive ? ' unselected' : ''}` // eslint-disable-line
-        }
+        className={(isActive) => `hidden sm:flex items-center gap-3 ${!isActive ? ' unselected' : ''}`}
       >
         <img className="w-24" src={img} alt="logo" />
         <h1 className="text-2xl font-bold">Space Xplore</h1>
