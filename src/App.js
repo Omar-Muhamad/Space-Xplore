53f52b7b4652012b1fce177f0e19c987b0edd97b
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-01-26 22:31:29 +0200

    Add reserved rockets to profile page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getAllMissions } from './Redux/missions/missions';
+import { getRockets } from './Redux/APIHelper';
 import Header from './Components/Header';
 import Rockets from './Components/Rockets';
 import Missions from './Components/Missions';
@@ -12,6 +13,7 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(getRockets());
     dispatch(getAllMissions());
   }, [dispatch]);
 
