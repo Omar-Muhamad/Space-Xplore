03f58a9688447cb4b4bc198d663e6dc0a1a9d9b4
George Magdy <georgtriple@gmail.com>
2022-01-26 11:22:07 +0200

    Render missions inside home page

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => <div>Under construction</div>;
+const Profile = () => {
+  const missions = useSelector((state) => state.missions);
+  const filteredMissions = missions.filter((mission) => mission.reserved);
+  console.log(filteredMissions);
+  return (
+    <div className="grid grid-cols-2">
+      <div className="px-5">
+        <h2 className="font-bold text-2xl px-1 py-2">My Missions</h2>
+        <table className="border-collapse border-2 border-slate-300 w-full">
+          <tbody>
+            {filteredMissions.map((mission) => (
+              <tr
+                className="border-2 border-slate-300"
+                key={mission.mission_id}
+              >
+                <td className="p-3">{mission.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>Hello</div>
+    </div>
+  );
+};
 
 export default Profile;
